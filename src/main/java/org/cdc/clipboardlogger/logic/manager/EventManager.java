8f0dc8853210356b97a4baea7471bae25305cd18
Administrator <3154934427@qq,com>
2021-08-07 11:33:06 +0800

    v1.1

---
+++ src/main/java/org/cdc/clipboardlogger/logic/manager/EventManager.java
@@ -0,0 +1,20 @@
+package org.cdc.clipboardlogger.logic.manager;
+
+import org.cdc.clipboardlogger.logic.eventcreator.EventCreator;
+import org.cdc.clipboardlogger.logic.eventcreator.MouseEventCreator;
+import org.cdc.clipboardlogger.logic.listener.ClipBoardLoggerMouseMotionListener;
+
+import java.util.logging.Logger;
+
+public class EventManager {
+    public static void EventRun() throws InstantiationException, IllegalAccessException {
+        //事件注册
+        for (Class<?> classInstance:JavaManager.getClasses("org.cdc.clipboardlogger.logic.eventcreator")){
+            if (classInstance.isInterface()||classInstance.isAnnotation()||"org.cdc.clipboardlogger.logic.eventcreator.EventCreator".equals(classInstance.getName())) continue;
+            EventCreator.createEvent((EventCreator) classInstance.newInstance());
+            Logger.getGlobal().info("注册事件"+classInstance.getSimpleName().replace("Creator",""));
+        }
+        //注册事件
+        MouseEventCreator.registerEvent(ClipBoardLoggerMouseMotionListener.getInstance());
+    }
+}
--- src/main/java/org/cdc/clipboardlogger/logic/manager/ProcessManager.java
+++ src/main/java/org/cdc/clipboardlogger/logic/manager/ProcessManager.java
@@ -5,6 +5,8 @@ import sun.jvmstat.monitor.*;
 import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 import java.net.URISyntaxException;
 import java.util.HashSet;
 import java.util.Set;
@@ -14,10 +16,25 @@ import java.util.logging.Logger;
  * 进程管理
  */
 public class ProcessManager {
-	public static boolean checkExist() throws IOException, URISyntaxException, MonitorException {
-		 if (checkFileExist()) return false;
-		return !checkProcessExist();
+	public static void processCheck() throws IOException, URISyntaxException, MonitorException {
+		Logger.getGlobal().info("正在检测本程序是否已经存在进程");
+		if (ProcessManager.checkExist()) {
+			JOptionPane.showMessageDialog(null, "程序已经运行，无法多开");
+			Logger.getGlobal().warning("程序已经存在进程,退出程序");
+			System.exit(0);
+		}
+		Logger.getGlobal().info("检测完毕,确定运行");
 	}
+	public static boolean checkExist() throws URISyntaxException, MonitorException {
+			return !checkProcessExist();
+	}
+
+	/**
+	 * 以往的检测模式
+	 * @return 是否存在进程
+	 * @throws IOException
+	 */
+	@Deprecated
 	private static boolean checkFileExist() throws IOException {
 		File run=new File("run");
 		if (run.exists()) return false;
@@ -35,15 +52,26 @@ public class ProcessManager {
 		MonitoredHost local = MonitoredHost.getMonitoredHost("localhost");
 		// 取得所有在活动的虚拟机集合
 		Set<?> vmlist = new HashSet<Object>(local.activeVms());
+		Logger.getGlobal().info("监听到jvm主机"+vmlist);
+		MonitoredVm currentVm = local.getMonitoredVm(new VmIdentifier("//" + getProcessID()));
+		String currentProcessName = MonitoredVmUtil.mainClass(currentVm, true);
 		int count=0;
 		for(Object process : vmlist) {
 			MonitoredVm vm = local.getMonitoredVm(new VmIdentifier("//" + process));
 			// 获取类名
-			String processname = MonitoredVmUtil.mainClass(vm, true);
-			if ("org.cdc.clipboardlogger.Main".equals(processname)){
+			String processName = MonitoredVmUtil.mainClass(vm, true);
+			Logger.getGlobal().info(processName);
+			if (currentProcessName.equals(processName)){
 				count+=1;
+				Logger.getGlobal().info("监听到进程:"+processName);
 			}
 		}
-		return count==1;
+		return count<=1;
+	}
+	public static int getProcessID() {
+		RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+		System.out.println(runtimeMXBean.getName());
+		return Integer.valueOf(runtimeMXBean.getName().split("@")[0])
+				;
 	}
 }
